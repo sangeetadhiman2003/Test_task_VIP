56e5ec93a549acbd8c3f9e940fb59cf5869283d7
Arvind Singh <arvind.b@ksolves.com>
2026-03-09 12:16:27 +0530

    added migeration form order, sku and line-item

--- db/migrate/20260309061933_create_line_items.rb
+++ db/migrate/20260309061933_create_line_items.rb
@@ -2,11 +2,8 @@ class CreateLineItems < ActiveRecord::Migration[7.0]
   def change
     create_table :line_items do |t|
       t.string :sku
-      t.string :string
-      t.string :quantity
-      t.string :integer
-      t.string :original
-      t.string :boolean
+      t.integer :quantity
+      t.boolean :original
       t.references :order, null: false, foreign_key: true
 
       t.timestamps
